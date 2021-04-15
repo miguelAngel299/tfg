5bb8f7b33857cfcfda599a9c4e77998599c9b5a3
MIGUEL ANGEL PICAZO FERNANDEZ <MiguelAngel.Picazo1@alu.uclm.es>
2021-04-15 16:52:06 +0200

    Camera JS y FileChooser Java

--- index.js
+++ index.js
@@ -6,3 +6,19 @@ app.use(express.static(__dirname + '/p5/'));
 app.listen('3000', function() {
   console.log('Servidor web escuchando en el puerto 3000');
 });
+/*
+var spawn = require('child_process').spawn;
+var prc = spawn('java',  ['-jar', '-Xmx512M', '-Dfile.encoding=utf8', 'programaJava/binaryConverter.jar']);
+
+//noinspection JSUnresolvedFunction
+prc.stdout.setEncoding('utf8');
+prc.stdout.on('data', function (data) {
+    var str = data.toString()
+    var lines = str.split(/(\r?\n)/g);
+    console.log(lines.join(""));
+});
+
+prc.on('close', function (code) {
+    console.log('process exit code ' + code);
+});
+*/
