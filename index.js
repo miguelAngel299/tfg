0c81b7f0ceadedd44dace6559811f6ab25b2ef27
MIGUEL ANGEL PICAZO FERNANDEZ <MiguelAngel.Picazo1@alu.uclm.es>
2021-06-23 11:57:01 +0200

    setPort 5000

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use(express.static(__dirname + '/'));
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 
-app.set('port', process.env.PORT || 80);
+app.set('port', process.env.PORT || 5000);
 
 //app.use(express.static(__dirname + '/'));
 app.use(express.static(__dirname)); 
